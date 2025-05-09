4c671f76304bc0e6258273ae12c6c6c6a7ee2852
Alberto Rey <ajrey1282@gmail.com>
2025-05-09 16:59:40 -0500

    feat: add component card and photo pokemon

---
+++ src/app/components/photo-pokemon/photo-pokemon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-pokemon',
+  imports: [],
+  templateUrl: './photo-pokemon.component.html',
+  styleUrl: './photo-pokemon.component.scss'
+})
+export class PhotoPokemonComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { PhotoPokemonComponent } from "../../components/photo-pokemon/photo-pokemon.component";
+import { CardPokemonComponent } from "../../components/card-pokemon/card-pokemon.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [PhotoPokemonComponent, CardPokemonComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
